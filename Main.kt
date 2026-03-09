5000fd4c61d44efad245c34636833d56d049c217
Nouribrahim88 <ibrahemnour351@gmail.com>
2026-03-09 03:28:48 +0200

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,11 +1,15 @@
 fun main() {
-    val score=readln().toInt()
-    when (score) {
-        in 90..100 ->println("A")
-        in 80 .. 89 ->println("B")
-        in 70 .. 79 ->println("C")
-        else -> println("F")
+    var name : String = ""
+    var nickname: String?=null
+    var age: Int=0
+
+    if(nickname==null)
+    {
+        println("nickname error")
     }
+    if (age < 15) println("Child")
+    else if ( age < 18) println("Teen")
+    else println("Adult")
 }
 
 
