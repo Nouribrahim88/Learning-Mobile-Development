062ef36869fe6e7c6b4debb723f48ce8e270ba49
Nouribrahim88 <ibrahemnour351@gmail.com>
2026-03-10 02:54:58 +0200

    Add files via upload

---
+++ NullSafe.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name : String = ""
+    var nickname: String?=null
+    var age: Int=0
+
+    if(nickname==null)
+    {
+        println("nickname error")
+    }
+    if (age < 15) println("Child")
+    else if ( age < 18) println("Teen")
+    else println("Adult")
+}
